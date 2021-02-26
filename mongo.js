9f531047c3120b31b80f7fbb8579e3177ece0b88
Muhammad Hamza <hamzamustafa2k@gmail.com>
2021-02-27 01:00:48 +0500

    fun

--- mongo.js
+++ mongo.js
@@ -1,4 +1,21 @@
 //requiring mongoose library for connecting database mongodb atlas
+var somevar = false;
+var PTest = function () {
+    return new Promise(function (resolve, reject) {
+        if (somevar === true)
+            resolve();
+        else
+            reject();
+    });
+}
+var myfunc = PTest();
+myfunc.then(function () {
+     console.log("Promise Resolved");
+});
+// See the Difference here
+myfunc.catch(function () {
+     console.log("Promise Rejected");
+});
 const mongoose = require("mongoose");
 require("dotenv").config();
 
